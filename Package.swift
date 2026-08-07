04da7893b9138cd44f39c3d363ceb2bb671e2458
denysraptive <dvorko@raptive.com>
2026-08-07 09:27:33 +0200

    Release/1.1.0 (#2)
    * Release 1.1.0
    
    * Update README
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -31,13 +31,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "RaptiveAdsSDK",
-            url: "https://raptive-maa-packages-development.s3.us-east-1.amazonaws.com/ios/releases/1.0.0/RaptiveAdsSDK-1.0.0.xcframework.zip",
-            checksum: "85ab1c286965432d8ba96eaf73b5c160aff65b619903a1a5571b8bbd4727ac81"
+            url: "https://raptive-maa-packages-development.s3.us-east-1.amazonaws.com/ios/releases/1.1.0/RaptiveAdsSDK-1.1.0.xcframework.zip",
+            checksum: "54b94ecf6fa9869d853e38c55b9b8073f858cfedbfbb3bfbd764c2abb2118172"
         ),
         .binaryTarget(
             name: "PrebidMobileOMSDK",
-            url: "https://raptive-maa-packages-development.s3.us-east-1.amazonaws.com/ios/releases/1.0.0/OMSDK_Prebidorg-1.0.0.xcframework.zip",
-            checksum: "f8ecd211787d9e9aa5285665da6a25b01aad0ef8fc0fdd83b123cc5d3f5a931d"
+            url: "https://raptive-maa-packages-development.s3.us-east-1.amazonaws.com/ios/releases/1.1.0/OMSDK_Prebidorg-1.1.0.xcframework.zip",
+            checksum: "13977c2abed964f0512ae99f9db29ea149ab90e92112f47159c9f83977deb76f"
         )
     ]
 )
